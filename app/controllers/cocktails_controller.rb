96b64a670ed7f324cfe9604eefb481460ed41b88
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-10 15:55:05 +0200

    toute la base est prete, reste plus que le front

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,23 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name))
+    # we need `restaurant_id` to asssociate cocktail with corresponding restaurant
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,21 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(params.require(:dose).permit(:description, :cocktail_id, :ingredient_id))
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
 end
